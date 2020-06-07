81149bc22e9819f66ee473399bab7590cafa2a53
Jordi Puigdellívol <jordi.p@revo.works>
2020-06-07 15:31:33 +0200

    Initial Commit

---
+++ RevoValidation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RevoValidation
+//
+//  Created by Jordi Puigdellívol on 07/06/2020.
+//  Copyright © 2020 Jordi Puigdellívol. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
