5c615b31f19003ed0ec78129baf5c8c823593a51
Miquel Rodoreda <miquel.rodoreda@revo.works>
2024-06-06 12:19:16 +0200

    Improve RegEx

--- RevoValidation/src/Rules/RuleUrl.swift
+++ RevoValidation/src/Rules/RuleUrl.swift
@@ -5,17 +5,9 @@ public class RuleUrl : Rule {
     override var errorMessage: String { "Needs to be an url" }
 
     override public func isValid(_ text:String) -> Bool {
-        guard !text.contains("..")                    else { return false }
-        guard !text.contains(" ")                     else { return false }
-        guard (text.first != "." && text.last != ".") else { return false }
-
-        let head     = "(((http|https)://)?([(w|W)]{3}+\\.)?)?"
-        let tail     = "\\.+[A-Za-z]{2,}+(\\.)?+(/(.)*)?"
-        let urlRegEx = head + "+(.)+" + tail
-
+        let urlRegEx = "^(https?://)?(www\\.)?([a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,}"
         let urlPred = NSPredicate(format:"SELF MATCHES %@", urlRegEx)
-        let isUrl = urlPred.evaluate(with: text)
-
-        return isUrl
+        
+        return urlPred.evaluate(with: text)
     }
 }
