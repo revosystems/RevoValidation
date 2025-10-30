e858c0dffa152f015f84e5a0e63f917e9fb59c6e
Jordi Puigdellívol <jordipuigdellivol@Jordis-Mac-mini.local>
2025-10-30 09:23:35 +0100

    Creating the form validator to be able to validate a few fields

---
+++ RevoValidation/src/SwiftUI/FormValidator.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+import RevoValidation
+import Combine
+
+public class FormValidator : ObservableObject {
+    @Published public var isValid: Bool = false
+    
+    
+    
+    
+    public init(){
+    }
+}
--- RevoValidation/src/SwiftUI/RulesViewModifier.swift
+++ RevoValidation/src/SwiftUI/RulesViewModifier.swift
@@ -2,6 +2,7 @@ import SwiftUI
 
 @available(iOS 17.0, *)
 public struct RulesViewModifier : ViewModifier {
+    @Binding var validator:FormValidator
     @Binding var text:String
     let rules:Rules
     
@@ -10,14 +11,16 @@ public struct RulesViewModifier : ViewModifier {
     public func body(content: Content) -> some View {
         VStack(alignment: .leading) {
             content
-            if let invalid = invalidRules, !invalid.firstErrorMessage.isEmpty {
-                Text(invalid.firstErrorMessage)
+            if let invalid = invalidRules, !invalid.errorMessage.isEmpty {
+                Text(invalid.errorMessage)
                     .foregroundStyle(.secondary)
                     .font(.caption)
             }
         }
         .onChange(of: text) { _, newValue in
             invalidRules = rules.validate(newValue)
+            validator.isValid = (invalidRules?.count ?? 0) == 0
+            //validator.errors[text] = invalidRules
         }
         .overlay(alignment:.topTrailing) {
             if (invalidRules?.count ?? 0) > 0 {
@@ -30,17 +33,25 @@ public struct RulesViewModifier : ViewModifier {
 }
 
 public extension TextField {
-    func rules(_ text:Binding<String>, _ rules:[Rule]) -> some View {
+    func rules(_ text:Binding<String>, _ rules:[Rule], formValidator:Binding<FormValidator>) -> some View {
         if #available(iOS 17.0, *) {
-            return modifier(RulesViewModifier(text:text, rules: Rules(rules)))
+            return modifier(RulesViewModifier(
+                validator:formValidator,
+                text:text,
+                rules: Rules(rules))
+            )
         } else {
             return self
         }
     }
     
-    func rules(_ text:Binding<String>, _ rules:String) -> some View {
+    func rules(_ text:Binding<String>, _ rules:String, formValidator:Binding<FormValidator>) -> some View {
         if #available(iOS 17.0, *) {
-            return modifier(RulesViewModifier(text:text, rules: Rules(stringLiteral: rules)))
+            return modifier(RulesViewModifier(
+                validator:formValidator,
+                text:text,
+                rules: Rules(stringLiteral: rules)
+            ))
         } else {
             return self
         }
